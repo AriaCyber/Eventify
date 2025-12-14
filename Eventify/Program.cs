1b4866789278eeac045309a61bf9b563ca53f20f
Abhinav Ajayakumar <abhix71@gmail.com>
2025-12-13 20:21:04 -0500

    fix(api): enable CORS AcceptAll for local dev (and keep HTTPS redirect off)

--- Eventify/Program.cs
+++ Eventify/Program.cs
@@ -2,9 +2,18 @@ using Eventify.Data;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
+
+// CORS
+builder.Services.AddCors(o => o.AddPolicy("AcceptAll",
+    p => p.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader()));
+
 builder.Services.AddDbContext<AppDbContext>();
 builder.Services.AddControllers();
+
 var app = builder.Build();
-app.UseHttpsRedirection();
+
+// app.UseHttpsRedirection();   
+app.UseCors("AcceptAll");
+
 app.MapControllers();
 app.Run();
